9c473dda45a5fa3da95615b0494efaee79e22848
manjula1711 <141096624+manjula1711@users.noreply.github.com>
2026-02-27 11:47:28 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,28 @@
-// src/api/axios.js
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:8080/api",
-   baseURL:"https://jobportalsystem-4fu4.onrender.com/api",
+  baseURL: "https://jobportalsystem-4fu4.onrender.com/api",
 });
 
-// attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  const url = config.url || "";
+
+  const publicRoutes = [
+    "/auth/login",
+    "/auth/register",
+    "/auth/forgot-password",
+    "/auth/reset-password",
+  ];
+
+  const isPublic = publicRoutes.some((r) => url.includes(r));
+
+  if (!isPublic && token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    delete config.headers.Authorization;
+  }
+
   return config;
 });
 
