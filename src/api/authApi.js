77f631970b3bbf8aa3a4431f318d2f84a29f0c11
manjula1711 <141096624+manjula1711@users.noreply.github.com>
2026-02-27 11:54:02 +0530

    Update authApi.js

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,13 +1,11 @@
-// src/api/authApi.js
-import api from "./axios";
-
 import publicApi from "./publicApi";
 
-// ✅ Public endpoints (NO token)
 export const registerApi = (data) => publicApi.post("/auth/register", data);
+
 export const loginApi = (data) => publicApi.post("/auth/login", data);
+
 export const forgotPasswordApi = (email) =>
   publicApi.post("/auth/forgot-password", { email });
+
 export const resetPasswordApi = (token, newPassword) =>
   publicApi.post("/auth/reset-password", { token, newPassword });
-
